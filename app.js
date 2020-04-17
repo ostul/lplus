54d13a5a142e88b8d417968ff3b1efca4e08dedd
ostul <ostul@softserveinc.com>
2020-04-17 14:36:25 +0300

    init

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 exports.TodoPage = require('./cypress/page_objects/todo-page').TodoPage
+
+exports.HomePage = {
+    login: () => {
+        cy.log('Log In')
+    }
+}
+exports.LoginPage = {
+    logout: () => {
+        cy.log('Log Out')
+    }
+}
